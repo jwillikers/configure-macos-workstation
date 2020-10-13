50828fab8fab65d52d35c7d649e73a8c846f70e1
Jordan Williams <19399197+jwillikers@users.noreply.github.com>
2020-10-13 09:11:09 -0500

    Use single quotes to avoid escaping double quotes

--- src/bat.fish
+++ src/bat.fish
@@ -1,6 +1,6 @@
 sudo port install bat;
 and bat --generate-config-file;
-and echo "--theme=\"Solarized (dark)\"" >> (bat --config-file);
+and echo '--theme="Solarized (dark)"' >> (bat --config-file);
 and set -Ux LESS "-R";
 and set -Ux LESSOPEN "| bat --color always %s";
 and set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'";
