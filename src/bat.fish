6595abf79588da87dc0f97f046b5d77435834975
Jordan Williams <19399197+jwillikers@users.noreply.github.com>
2020-10-13 09:10:47 -0500

    When aliasing cat, find the bat executable on the path
    This is slightly more flexible in case bat is installed elsewhere.

--- src/bat.fish
+++ src/bat.fish
@@ -4,4 +4,4 @@ and echo "--theme=\"Solarized (dark)\"" >> (bat --config-file);
 and set -Ux LESS "-R";
 and set -Ux LESSOPEN "| bat --color always %s";
 and set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'";
-and alias -s cat /opt/local/bin/bat
+and alias -s cat (which bat)
